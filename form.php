2ad62ebdfcb48c1fb55421b80031a49cf73072c5
anuj-c <57581295+anuj-c@users.noreply.github.com>
2020-11-23 12:10:49 +0530

    Change in form submitting method

--- form.php
+++ form.php
@@ -1,67 +1,51 @@
-<html>
-    <head>
-        <title>Form</title>
-    </head>
-    <style>
-        body{
-            background: skyblue;
-        }
-        h1{
-            text-align:center;
-            margin-top:40vh;
-        }
-    </style>
-    <body>
-        <?php
-            $name=$email=$message="";
-            if($_SERVER["REQUEST_METHOD"]=="POST")
-            {
-                $name=test_input($_POST["name"]);
-                $email=test_input($_POST["mail"]);
-                $message=test_input($_POST["message"]);
-            }
-            function test_input($data){
-                $data=trim($data);
-                $data=stripslashes($data);
-                $data=htmlspecialchars($data);
-                return $data;
-            }
+<?php
+    $name=$email=$message="";
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+        $name=test_input($_POST["name"]);
+        $email=test_input($_POST["mail"]);
+        $message=test_input($_POST["message"]);
+    }
+    function test_input($data){
+        $data=trim($data);
+        $data=stripslashes($data);
+        $data=htmlspecialchars($data);
+        return $data;
+    }
 
-            use PHPMailer\PHPMailer\PHPMailer;
-            use PHPMailer\PHPMailer\SMTP;
-            use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 
-            require "PHPMailer/PHPMailer.php";
-            require "PHPMailer/SMTP.php";
-            require "PHPMailer/Exception.php";
+    require "PHPMailer/PHPMailer.php";
+    require "PHPMailer/SMTP.php";
+    require "PHPMailer/Exception.php";
 
-            $mail = new PHPMailer(true);
+    $mail = new PHPMailer(true);
 
-            try {
-                $mail->isSMTP();
-                $mail->Host       = 'smtp.gmail.com';
-                $mail->SMTPAuth   = true;
-                $mail->Username   = '';       //  SENDERS MAIL
-                $mail->Password   = '';   //  SENDERS PASSWORD
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port       = 587;
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = '';       //  SENDERS MAIL
+        $mail->Password   = '';   //  SENDERS PASSWORD
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = 587;
 
-                $mail->setFrom('');       //  SENDERS MAIL
-                $mail->addAddress('');  //  RECEIVERS MAIL
-                // $mail->addReplyTo($email);      //  PARTICIPANT'S MAIL TO REPLY DIRECTLY
+        $mail->setFrom('');       //  SENDERS MAIL
+        $mail->addAddress('');  //  RECEIVERS MAIL
+        // $mail->addReplyTo($email);      //  PARTICIPANT'S MAIL TO REPLY DIRECTLY
 
-                $mail->isHTML(true);
-                $mail->Subject = 'Esummit Form';
-                $mail->Body    = "<ul><li>Name = $name</li>";
-                $mail->Body .= "<li>E-Mail = $email</li>";
-                $mail->Body .= "<li>Message :</li><ul>$message</ul></ul>";
+        $mail->isHTML(true);
+        $mail->Subject = 'Esummit Form';
+        $mail->Body    = "<ul><li>Name = $name</li>";
+        $mail->Body .= "<li>E-Mail = $email</li>";
+        $mail->Body .= "<li>Message :</li><ul>$message</ul></ul>";
 
-                $mail->send();
-                header('Location:index.html');
-            } 
-            catch (Exception $e) {
-                echo "<h1>Message could not be sent....<br>Please check internet connection</h1>";
-            }
-        ?>
-    </body>
-</html>
+        $mail->send();
+        echo "Thankyou for submitting";
+    } 
+    catch (Exception $e) {
+        echo "alert";
+    }
+?>
